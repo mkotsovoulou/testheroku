942bfc69b352aef23d79853a577f39d98151e386
Maira Kotsovoulou <mkotsovoulou@acg.edu>
2022-03-24 16:42:39 +0200

    procFile

--- app/conference/urls.py
+++ app/conference/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    #path ("", views.index, name="index"),
+    path ("", include('fullstack.urls')),
     path('fullstack/', include('fullstack.urls')),
 ]
